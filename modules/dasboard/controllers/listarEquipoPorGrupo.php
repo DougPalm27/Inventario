e166929dd77876a98f50c75816994afb884f327a
MarcosSorto <marcoselisorto.me@hotmail.com>
2024-07-16 15:01:55 -0600

    Finalización de controladores y métodos para el dashboard principal

---
+++ modules/dasboard/controllers/listarEquipoPorGrupo.php
@@ -0,0 +1,10 @@
+ <!-- método para listar equipo por proyecto -->
+
+ <?php
+    include_once "../../../config/Connection.php";
+    include_once "../models/dash.php";
+    // Instanciamos el modelo y llamamos al método correspondiente
+    $conexion = new mdlDash();
+    $losDatos = $conexion->equipoPorGrupo();
+    echo json_encode($losDatos);
+?>
